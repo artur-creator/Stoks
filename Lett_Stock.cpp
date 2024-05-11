10fb25c7b6e40f3fee9d8455da64e60fc64b83b9
artur-creator <taurusspr@gmail.com>
2024-05-11 18:35:36 +0300

    fixing

--- Lett_Stock.cpp
+++ Lett_Stock.cpp
@@ -6,14 +6,16 @@
 #include <algorithm>
 using namespace std;
 
-int maxProfit(vector<int>& prices) {
+int maxProfit(vector<int>& prices) 
+{
+    int buyPrice = prices[0];
     int profit = 0;
-    int mini = prices[0];
     for (int i = 1; i < prices.size(); i++) {
+        if (buyPrice > prices[i]) {
+            buyPrice = prices[i];
+        }
 
-        int diff = prices[i] - mini;
-        profit = max(profit, diff);
-        mini = min(mini, prices[i]);
+        profit = max(profit, prices[i] - buyPrice);
     }
 
     return profit;
